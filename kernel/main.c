147278d1e5b2f58ef8bfa9a8b768f576bbc181f2
Yuan-Yao Sung <yysung@cs.nctu.edu.tw>
2019-03-05 18:36:02 +0800

    BSS init

--- kernel/main.c
+++ kernel/main.c
@@ -6,20 +6,31 @@
 #include <kernel/trap.h>
 #include <kernel/picirq.h>
 
+void bss_init() {
+    extern volatile char _BSS_START_[], bootstack[], bootstacktop[], _DATA_END_[];
+    for (volatile char *ptr = _BSS_START_; ptr != bootstack; ptr++) {
+        *ptr = 0;
+    }
+    for (volatile char *ptr = bootstacktop; ptr != _DATA_END_; ptr++) {
+        *ptr = 0;
+    }
+}
+
 extern void init_video(void);
 void kernel_main(void)
 {
-	init_video();
+    bss_init();
+    init_video();
 
-	pic_init();
-  /*       You should uncomment them
-   */
-	kbd_init();
-	timer_init();
-	trap_init();
+    pic_init();
+    /*       You should uncomment them
+    */
+    kbd_init();
+    timer_init();
+    trap_init();
 
-	/* Enable interrupt */
-	__asm __volatile("sti");
+    /* Enable interrupt */
+    __asm __volatile("sti");
 
-	shell();
+    shell();
 }
